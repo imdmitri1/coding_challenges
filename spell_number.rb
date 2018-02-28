eef7bc732bc8ef039a9c76310aad0ea97e2555c7
Dmitri Stefanisin <email.ds@protonmail.com>
2018-02-28 10:00:26 -0600

    spell any number up to a trillion

--- spell_number.rb
+++ spell_number.rb
@@ -48,3 +48,141 @@
 # 1002 becomes "one thousand and two".
 # 1323 becomes "one thousand three hundred and twenty-three".
 #
+
+
+def spell_one_nine(num)
+  case num
+  when 1
+    "one"
+  when 2
+    "two"
+  when 3
+    "three"
+  when 4
+    "four"
+  when 5
+    "five"
+  when 6
+    "six"
+  when 7
+    "seven"
+  when 8
+    "eight"
+  when 9
+    "nine"
+  end
+end
+
+def spell_ten_nineteen(num)
+  case num
+  when 10
+    "ten"
+  when 11
+    "eleven"
+  when 12
+    "twelve"
+  when 13
+    "thirteen"
+  when 14
+    "fourteen"
+  when 15
+    "fifteen"
+  when 16
+    "sixteen"
+  when 17
+    "seventeen"
+  when 18
+    "eighteen"
+  when 19
+    "nineteen"
+  end
+end
+
+def spell_tens(num)
+  case num
+  when 2
+    "twenty"
+  when 3
+    "thirty"
+  when 4
+    "forty"
+  when 5
+    "fifty"
+  when 6
+    "sixty"
+  when 7
+    "seventy"
+  when 8
+    "eighty"
+  when 9
+    "ninety"
+  end
+end
+
+def spell_one_ninety_nine(num)
+  if num / 10 >= 2
+    "#{spell_tens(num / 10)} #{spell_one_nine(num % 10)}"
+  elsif num / 10 == 1
+    spell_ten_nineteen(num)
+  else
+    spell_one_nine(num)
+  end
+end
+
+def spell_one_to_999(num)
+  if num / 100 > 0
+    "#{spell_one_nine(num / 100)} hundred and #{spell_one_ninety_nine(num % 100)}"
+  else
+    spell_one_ninety_nine(num)
+  end
+end
+
+def spell_upto_million(num)
+  if num / 1_000 > 0
+    "#{spell_one_to_999(num / 1_000)} thousand #{spell_one_to_999(num % 1_000)}"
+  else
+    spell_one_to_999(num)
+  end
+end
+
+def spell_upto_billion(num)
+  if num / 1_000_000 > 0
+    "#{spell_one_to_999(num / 1_000_000)} million #{spell_upto_million(num % 1_000_000)}"
+  else
+    spell_upto_million(num)
+  end
+end
+
+def spell_upto_trillion(num)
+  return "zero" if num == 0
+  return "outside of range" unless (0..999_999_999_999).include?(num)
+  if num / 1_000_000_000 > 0
+    "#{spell_one_to_999(num / 1_000_000_000)} billion #{spell_upto_billion(num % 1_000_000_000)}"
+  else
+    spell_upto_billion(num)
+  end
+end
+
+p spell_upto_trillion(ARGV[0].to_i) if ARGV[0]
+
+p "********** 10 random numbers ************"
+p a = rand(0..99)
+p spell_upto_trillion(a)
+p b = rand(0..999)
+p spell_upto_trillion(b)
+p c = rand(0..99_999)
+p spell_upto_trillion(c)
+p d = rand(0..999_999)
+p spell_upto_trillion(d)
+p e = rand(0..9_999_999)
+p spell_upto_trillion(e)
+p f = rand(0..99_999_999)
+p spell_upto_trillion(f)
+p j = rand(0..9_999_999_999)
+p spell_upto_trillion(j)
+p g = rand(0..99_999_999_999)
+p spell_upto_trillion(g)
+p i = rand(0..999_999_999_999)
+p spell_upto_trillion(i)
+p k = rand(0..999_999_999_999)
+p spell_upto_trillion(k)
